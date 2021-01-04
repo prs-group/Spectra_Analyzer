69c57a68277d48972d3b508ff408b17ebb4e798c
Pericyclic <72654154+Pericyclic@users.noreply.github.com>
2021-01-04 17:51:17 +0100

    changed intergation

--- Spectra_Analyzer_1_2.py
+++ Spectra_Analyzer_1_2.py
@@ -17,9 +17,10 @@ import scipy
 from scipy import integrate
 
 
-#Version 1.1 24.11.20
+#Version 1.2 04.12.20
 #Changelog:
-#only one band also possible right now
+#fit can be disabled
+#simps integration got replaced by trapz integration due to an integration error when using simps
 #    _._     _, -'""`-._
 #   (,-.`._,'(  MR   |\`-/
 #       `-.-' \ )-`( , o o)
@@ -29,15 +30,16 @@ from scipy import integrate
 
 #-------------------------
 #Change Variables here 
-how_often = 359 #number of time_vars, files, imports = last number + 1 so if BB_137.0270.csv is the last file, then 271
-name_sample = "BB_141" #Samplenumber for "Samplenumber.xxxx.csv"
+how_often = 37 #number of time_vars, files, imports = last number + 1 so if BB_137.0270.csv is the last file, then 271
+name_sample = "BB_101" #Samplenumber for "Samplenumber.xxxx.csv"
 Spectra_cut = False  #cut in spectra data?
+break_time_var = 19 #how long was the break time_var in h
 cut_number = 124 #Last number of first data set +1, e.g. BB_137.0123.csv is the last, then choose 124
 want_legend = True
-break_time_var = 19 #how long was the break time_var in h
-want_animation = True #want animation? Gives Animation of both bands
-second_plot = False
+want_animation = False #want animation? Gives Animation of both bands
+second_plot = True
 interval_time_var = 1 #time_var between measurements in h 
+want_fit = True #just to test of the data is corrupted
 
 #modelfit_parameters #use if NaN error occours 
 #model1: a*np.exp(x*k) + b
@@ -49,11 +51,11 @@ param_A = 1
 param_B = 0
 param_K = 0.01
 
-base_line_left = 1245.6
-base_line_right = 1251.3
+base_line_left = 852.0
+base_line_right = 858.5 
 
-base_line_left_2 = 99999
-base_line_right_2 = 99999
+base_line_left_2 = 735.0
+base_line_right_2 = 738.0
 
 # name of the files, file data like .eps, .gif ... has to be given
 file_format = "eps" #has to be set to the same as the format of the plot
@@ -77,14 +79,14 @@ color_animation = "#6388AC"
 color_animation_2 = "#6388AC"
 
 #name in legend for data points
-label_data_points = "Experimental Data at 1249.4 cm$^{-1}$"
-label_data_points_2 = "Experimental Data at 1688.5 cm$^{-1}$"
-size_legend = "x-small"
+label_data_points = "Experimental Data at 855 cm$^{-1}$"
+label_data_points_2 = "Experimental Data at 736 cm$^{-1}$"
+size_legend = "xx-small" # x-small, normal, or numerical values like 20 can be used
 #name for fit has to be changed in line 384 and 385
 
 #axis label plot
-ax1_label = "Peak-area at 1249.4 cm$^{-1}$ / a.u. "
-ax2_label = "Peak-area at 1688.5 cm$^{-1}$ / a.u. "
+ax1_label = "Peak-area at 855 cm$^{-1}$ / a.u. "
+ax2_label = "Peak-area at 736 cm$^{-1}$ / a.u. "
 x_axis_label = "Time / h"
 
 #axis label animation
@@ -133,33 +135,36 @@ area_list = []
 area_list_2 = []
 
 # Print iterations progress
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
-    # Print New Line on Complete
-    if iteration == total: 
-        print()
+want_progress = True #turn off if you don't want to see the progress
+if want_progress == True:
+    def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+        """
+        Call in a loop to create terminal progress bar
+        @params:
+            iteration   - Required  : current iteration (Int)
+            total       - Required  : total iterations (Int)
+            prefix      - Optional  : prefix string (Str)
+            suffix      - Optional  : suffix string (Str)
+            decimals    - Optional  : positive number of decimals in percent complete (Int)
+            length      - Optional  : character length of bar (Int)
+            fill        - Optional  : bar fill character (Str)
+            printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+        """
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+        # Print New Line on Complete
+        if iteration == total: 
+            print()
 
+if want_progress == True:       
+    printProgressBar(0, how_often, prefix = 'Progress:', suffix = 'Complete', length = 50)
 
-printProgressBar(0, how_often, prefix = 'Progress:', suffix = 'Complete', length = 50)
 for i in range(how_often):
     if i < 10:
         file_name = name_sample + "." + "000" + str(i) + ".csv"
-        with open(file_name, "r") as csvfile:
+        with open(file_name, "r", encoding="utf-8") as csvfile:
             reader = csv.reader(csvfile)
             x_list = []
             y_list = []
@@ -187,9 +192,9 @@ for i in range(how_often):
             straight_line_y_2 = [y_value_for_left_side_2, y_value_for_right_side_2]
             straight_line_x_2 = [base_line_left_2, base_line_right_2]
 
-        area_straight = scipy.integrate.simps(straight_line_y, straight_line_x, even='avg')
+        area_straight = np.trapz(straight_line_y, x=straight_line_x)
         if second_plot == True:
-            area_straight_2 = scipy.integrate.simps(straight_line_y_2, straight_line_x_2, even='avg')
+            area_straight_2 = np.trapz(straight_line_y_2, x=straight_line_x_2)
 
         #creating region of intrest
         x_values_peak = x_list[x_index_for_right_side: x_index_for_left_side + 1]
@@ -197,10 +202,10 @@ for i in range(how_often):
         if second_plot == True:
             x_values_peak_2 = x_list[x_index_for_right_side_2: x_index_for_left_side_2 + 1]
             y_values_peak_2 = y_list[x_index_for_right_side_2: x_index_for_left_side_2 + 1]
+        area_peak = np.trapz(y_values_peak, x=x_values_peak)
 
-        area_peak = scipy.integrate.simps(y_values_peak, x_values_peak, even='avg')
         if second_plot == True:
-            area_peak_2 = scipy.integrate.simps(y_values_peak_2, x_values_peak_2, even='avg')
+            area_peak_2 = np.trapz(y_values_peak_2, x=x_values_peak_2)
 
         #calculating difference
         area_effective = area_peak*(-1) - area_straight
@@ -210,7 +215,6 @@ for i in range(how_often):
             area_list_2.append(area_effective_2)
         if i == 0:
             plot_1_high_y_limit = area_effective*1.001
-
             if second_plot == True:
                 plot_2_low_y_limit = area_effective_2*0.975
         #clearance
@@ -245,9 +249,9 @@ for i in range(how_often):
             straight_line_y_2 = [y_value_for_left_side_2, y_value_for_right_side_2]
             straight_line_x_2 = [base_line_left_2, base_line_right_2]
 
-        area_straight = scipy.integrate.simps(straight_line_y, straight_line_x, even='avg')
+        area_straight = np.trapz(straight_line_y, x=straight_line_x)
         if second_plot == True:
-            area_straight_2 = scipy.integrate.simps(straight_line_y_2, straight_line_x_2, even='avg')
+            area_straight_2 = np.trapz(straight_line_y_2, x=straight_line_x_2)
 
         #creating region of intrest
         x_values_peak = x_list[x_index_for_right_side: x_index_for_left_side + 1]
@@ -255,10 +259,10 @@ for i in range(how_often):
         if second_plot == True:
             x_values_peak_2 = x_list[x_index_for_right_side_2: x_index_for_left_side_2 + 1]
             y_values_peak_2 = y_list[x_index_for_right_side_2: x_index_for_left_side_2 + 1]
-
-        area_peak = scipy.integrate.simps(y_values_peak, x_values_peak, even='avg')
+        area_peak = np.trapz(y_values_peak, x=x_values_peak)
+        
         if second_plot == True:
-            area_peak_2 = scipy.integrate.simps(y_values_peak_2, x_values_peak_2, even='avg')
+            area_peak_2 = np.trapz(y_values_peak_2, x=x_values_peak_2)
 
         #calculating difference
         area_effective = area_peak*(-1) - area_straight
@@ -303,9 +307,9 @@ for i in range(how_often):
             straight_line_y_2 = [y_value_for_left_side_2, y_value_for_right_side_2]
             straight_line_x_2 = [base_line_left_2, base_line_right_2]
 
-        area_straight = scipy.integrate.simps(straight_line_y, straight_line_x, even='avg')
+        area_straight = np.trapz(straight_line_y, x=straight_line_x)
         if second_plot == True:
-            area_straight_2 = scipy.integrate.simps(straight_line_y_2, straight_line_x_2, even='avg')
+            area_straight_2 = np.trapz(straight_line_y_2, x=straight_line_x_2)
 
         #creating region of intrest
         x_values_peak = x_list[x_index_for_right_side: x_index_for_left_side + 1]
@@ -313,10 +317,10 @@ for i in range(how_often):
         if second_plot == True:
             x_values_peak_2 = x_list[x_index_for_right_side_2: x_index_for_left_side_2 + 1]
             y_values_peak_2 = y_list[x_index_for_right_side_2: x_index_for_left_side_2 + 1]
-
-        area_peak = scipy.integrate.simps(y_values_peak, x_values_peak, even='avg')
+        area_peak = np.trapz(y_values_peak, x=x_values_peak)
+        
         if second_plot == True:
-            area_peak_2 = scipy.integrate.simps(y_values_peak_2, x_values_peak_2, even='avg')
+            area_peak_2 = np.trapz(y_values_peak_2, x=x_values_peak_2)
 
         #calculating difference
         area_effective = area_peak*(-1) - area_straight
@@ -361,9 +365,9 @@ for i in range(how_often):
             straight_line_y_2 = [y_value_for_left_side_2, y_value_for_right_side_2]
             straight_line_x_2 = [base_line_left_2, base_line_right_2]
 
-        area_straight = scipy.integrate.simps(straight_line_y, straight_line_x, even='avg')
+        area_straight = np.trapz(straight_line_y, x=straight_line_x)
         if second_plot == True:
-            area_straight_2 = scipy.integrate.simps(straight_line_y_2, straight_line_x_2, even='avg')
+            area_straight_2 = np.trapz(straight_line_y_2, x=straight_line_x_2)
 
         #creating region of intrest
         x_values_peak = x_list[x_index_for_right_side: x_index_for_left_side + 1]
@@ -371,10 +375,10 @@ for i in range(how_often):
         if second_plot == True:
             x_values_peak_2 = x_list[x_index_for_right_side_2: x_index_for_left_side_2 + 1]
             y_values_peak_2 = y_list[x_index_for_right_side_2: x_index_for_left_side_2 + 1]
-
-        area_peak = scipy.integrate.simps(y_values_peak, x_values_peak, even='avg')
+        area_peak = np.trapz(y_values_peak, x=x_values_peak)
+        
         if second_plot == True:
-            area_peak_2 = scipy.integrate.simps(y_values_peak_2, x_values_peak_2, even='avg')
+            area_peak_2 = np.trapz(y_values_peak_2, x=x_values_peak_2)
 
         #calculating difference
         area_effective = area_peak*(-1) - area_straight
@@ -392,64 +396,68 @@ for i in range(how_often):
         y_list.clear()
     else:
         continue
-
-    time.sleep(0.1)
-    # Update Progress Bar
-    printProgressBar(i + 1, how_often, prefix = 'Progress:', suffix = 'Complete', length = 50)
+    if want_progress == True:
+        time.sleep(0.1)
+        # Update Progress Bar
+        printProgressBar(i + 1, how_often, prefix = 'Progress:', suffix = 'Complete', length = 50)
 #-------------------------          
 
 
 #Fit function
 #-------------------------  
-def exponential(x, a, k, b):
-    return a*np.exp(x*k) + b
+if want_fit == True:
+    def exponential(x, a, k, b):
+        return a*np.exp(x*k) + b
 
-ExponentialModel = lmfit.Model(exponential)
-result = ExponentialModel.fit(area_list, x=time_var, a=param_a, k=param_k, b=param_b)
-#-------------------------
+    ExponentialModel = lmfit.Model(exponential)
+    result = ExponentialModel.fit(area_list, x=time_var, a=param_a, k=param_k, b=param_b)
+    #-------------------------
 
-#Fit function2
-#-------------------------
-if second_plot == True:
-    def exponential_2(X, A, K, B):
-        return  A*(1-np.exp(-K*X))+B
+    #Fit function2
+    #-------------------------
+    if second_plot == True:
+        def exponential_2(X, A, K, B):
+            return  A*(1-np.exp(-K*X))+B
 
-    exponentialModel_2 = lmfit.Model(exponential_2)
-    result_2 = exponentialModel_2.fit(area_list_2, X=time_var, A=param_A, B=param_B, K=param_K)
+        exponentialModel_2 = lmfit.Model(exponential_2)
+        result_2 = exponentialModel_2.fit(area_list_2, X=time_var, A=param_A, B=param_B, K=param_K)
 
 
-#search for parameters
-#-------------------------
-input_text = result.fit_report()
-k_crude = re.findall(r"k:[\n ]?[+-]?[\n ]?[0-9]*[\n ]*[0-9]*[\n ]*[.]?[\n ]*[0-9]+[\n ]*[0-9]+", input_text)
-k_calcul = float(k_crude[-1].replace("k: ", ""))
-k_const = round(k_calcul, 4)
-a_crude = re.findall(r"a:[\n ]?[+-]?[\n ]?[0-9]*[\n ]*[0-9]*[\n ]*[.]?[\n ]*[0-9]+[\n ]*[0-9]+", input_text)
-a_const = round(float(a_crude[-1].replace("a: ", "")), 4)
-b_crude = re.findall(r"b:[\n ]?[+-]?[\n ]?[0-9]*[\n ]*[0-9]*[\n ]*[.]?[\n ]*[0-9]+[\n ]*[0-9]+", input_text)
-b_const = round(float(b_crude[-1].replace("b: ", "")), 4)
-t_half = np.log(2)/(-k_const)
-t_half_round = round(t_half, 1)
-print(result.fit_report())
+    #search for parameters
+    #-------------------------
+    input_text = result.fit_report()
+    k_crude = re.findall(r"k:[\n ]?[+-]?[\n ]?[0-9]*[\n ]*[0-9]*[\n ]*[.]?[\n ]*[0-9]+[\n ]*[0-9]+", input_text)
+    k_calcul = float(k_crude[-1].replace("k: ", ""))
+    k_const = round(k_calcul, 4)
+    a_crude = re.findall(r"a:[\n ]?[+-]?[\n ]?[0-9]*[\n ]*[0-9]*[\n ]*[.]?[\n ]*[0-9]+[\n ]*[0-9]+", input_text)
+    a_const = round(float(a_crude[-1].replace("a: ", "")), 4)
+    b_crude = re.findall(r"b:[\n ]?[+-]?[\n ]?[0-9]*[\n ]*[0-9]*[\n ]*[.]?[\n ]*[0-9]+[\n ]*[0-9]+", input_text)
+    b_const = round(float(b_crude[-1].replace("b: ", "")), 4)
+    t_half = np.log(2)/(-k_const)
+    t_half_round = round(t_half, 1)
+    print(result.fit_report())
 
-if second_plot == True:
-    input_text2 = result_2.fit_report() #search for parameters
-    K_crude = re.findall(r"K:[\n ]?[+-]?[\n ]?[0-9]*[\n ]*[0-9]*[\n ]*[.]?[\n ]*[0-9]+[\n ]*[0-9]+", input_text2)
-    K_calcul = float(K_crude[-1].replace("K: ", ""))
-    K_const = round(K_calcul, 4)
-    A_crude = re.findall(r"A:[\n ]?[+-]?[\n ]?[0-9]*[\n ]*[0-9]*[\n ]*[.]?[\n ]*[0-9]+[\n ]*[0-9]+", input_text2)
-    A_const = round(float(A_crude[-1].replace("A: ", "")), 4)
-    B_crude = re.findall(r"B:[\n ]?[+-]?[\n ]?[0-9]*[\n ]*[0-9]*[\n ]*[.]?[\n ]*[0-9]+[\n ]*[0-9]+", input_text2)
-    B_const = round(float(B_crude[-1].replace("B: ", "")), 4)
-    t_half2 = np.log(2)/(-K_const)
-    t_half_round2 = round(-t_half2, 1)
-    print(result_2.fit_report())
+    if second_plot == True:
+        input_text2 = result_2.fit_report() #search for parameters
+        K_crude = re.findall(r"K:[\n ]?[+-]?[\n ]?[0-9]*[\n ]*[0-9]*[\n ]*[.]?[\n ]*[0-9]+[\n ]*[0-9]+", input_text2)
+        K_calcul = float(K_crude[-1].replace("K: ", ""))
+        K_const = round(K_calcul, 4)
+        A_crude = re.findall(r"A:[\n ]?[+-]?[\n ]?[0-9]*[\n ]*[0-9]*[\n ]*[.]?[\n ]*[0-9]+[\n ]*[0-9]+", input_text2)
+        A_const = round(float(A_crude[-1].replace("A: ", "")), 4)
+        B_crude = re.findall(r"B:[\n ]?[+-]?[\n ]?[0-9]*[\n ]*[0-9]*[\n ]*[.]?[\n ]*[0-9]+[\n ]*[0-9]+", input_text2)
+        B_const = round(float(B_crude[-1].replace("B: ", "")), 4)
+        t_half2 = np.log(2)/(-K_const)
+        t_half_round2 = round(-t_half2, 1)
+        print(result_2.fit_report())
+
 
 #Plot
 #-------------------------
-label_fit = "Best Fit: " + str(a_const) +" · exp(" + str(k_const) + "0 · $t$) + " + str(b_const) + "\nHalf-life = " + str(t_half_round) + " h"
-if second_plot == True:
-    label_fit_2 = "Best Fit: " + str(A_const) +" · (1 - exp(-" + str(K_const) + " · $t$) + " + str(B_const) + "\nHalf-life = " + str(t_half_round2) + " h"
+if want_fit == True:
+    label_fit = "Best Fit: " + str(a_const) +" · exp(" + str(k_const) + "0 · $t$) + " + str(b_const) + "\nHalf-life = " + str(t_half_round) + " h"
+    if second_plot == True:
+        label_fit_2 = "Best Fit: " + str(A_const) +" · (1 - exp(-" + str(K_const) + " · $t$) + " + str(B_const) + "\nHalf-life = " + str(t_half_round2) + " h"
+
 max_time_var = time_var[-1]
 #-------------------------
 
@@ -458,7 +466,8 @@ color = color_axis_plot
 ax1.set_xlabel(x_axis_label)
 ax1.set_ylabel(ax1_label, color=color_label_plot)
 plt1 = ax1.plot(time_var, area_list, 'o', markersize=1, color=color_plot_data_points, label=label_data_points)
-plt2 = ax1.plot(time_var, result.best_fit, color=color_plot_fit_curve,  linewidth=2, linestyle=':', label=label_fit)
+if want_fit == True:
+    plt2 = ax1.plot(time_var, result.best_fit, color=color_plot_fit_curve,  linewidth=2, linestyle=':', label=label_fit)
 ax1.tick_params(axis='y', labelcolor=color)
 ax1.axis([0, max_time_var, plot_1_low_y_limit, plot_1_high_y_limit])
 
@@ -467,17 +476,18 @@ if second_plot == True:
     color = color_axis_plot_2
     ax2.set_ylabel(ax2_label, color=color_label_plot_2)  # we already handled the x-label with ax1
     plt3 = ax2.plot(time_var, area_list_2,'o', markersize=1, color=color_plot_data_points_2, label=label_data_points_2)
-    plt4 = ax2.plot(time_var, result_2.best_fit, color=color_plot_fit_curve_2,  linewidth=2, linestyle=':', label=label_fit_2)
+    if want_fit == True:
+        plt4 = ax2.plot(time_var, result_2.best_fit, color=color_plot_fit_curve_2,  linewidth=2, linestyle=':', label=label_fit_2)
     ax2.tick_params(axis='y', labelcolor=color)
     ax2.axis([0, max_time_var, plot_2_low_y_limit, plot_2_high_y_limit])
-    if want_legend == True:
+    if want_legend == True and want_fit == True:
         fontP = FontProperties() #legend if 4 plots needed
         fontP.set_size(size_legend)
         plts = plt1 + plt2 + plt3 + plt4
         labs = [l.get_label() for l in plts]
         plt.legend(plts, labs, loc=7, prop=fontP)
 else:
-    if want_legend == True:#legend if only 2 plots needed
+    if want_legend == True and want_fit == True: #legend if only 2 plots needed
         fontP = FontProperties()
         fontP.set_size(size_legend)
         plts = plt1 + plt2
@@ -488,9 +498,10 @@ fig.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.savefig(name_plot_1, format=file_format)
 
 #-------------------------
-print("t_half = " +  str(t_half))
-if second_plot == True:
-    print("t_half = " +  str(-t_half2))
+if want_fit == True:
+    print("t_half = " +  str(t_half))
+    if second_plot == True:
+        print("t_half = " +  str(-t_half2))
 #-------------------------
 #ANIMATIONS
 
